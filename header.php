1819b2681a5af6a384218bc05cabed6214b0d1a9
mac <mac@MacBook-Pro-mac.local>
2026-01-14 18:22:38 +0200

    Update header: height 64px, menu with dropdown, Manrope font

--- header.php
+++ header.php
@@ -27,5 +27,27 @@
 				<img src="<?php echo esc_url( get_site_url() . '/wp-content/uploads/2026/01/GTS.svg' ); ?>" alt="<?php bloginfo( 'name' ); ?>">
 			</a>
 		</div>
+
+		<nav class="main-navigation">
+			<ul class="menu">
+				<li class="menu-item menu-item-has-children">
+					<a href="#" class="menu-link">
+						Services
+						<span class="dropdown-icon">
+							<img src="<?php echo esc_url( get_site_url() . '/wp-content/uploads/2026/01/Vector-2.svg' ); ?>" alt="">
+						</span>
+					</a>
+					<ul class="sub-menu">
+						<!-- Dropdown items will be added here -->
+					</ul>
+				</li>
+				<li class="menu-item"><a href="#" class="menu-link">Events</a></li>
+				<li class="menu-item"><a href="#" class="menu-link">For Business</a></li>
+				<li class="menu-item"><a href="#" class="menu-link">Fleet</a></li>
+				<li class="menu-item"><a href="#" class="menu-link">Blog</a></li>
+				<li class="menu-item"><a href="#" class="menu-link">Contacts</a></li>
+				<li class="menu-item"><a href="#" class="menu-link">About Us</a></li>
+			</ul>
+		</nav>
 	</div>
 </header>
