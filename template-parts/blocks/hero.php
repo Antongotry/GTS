6e7ab82e9d01880db3252db82b91ece38f2ac170
mac <mac@MacBook-Pro-mac.local>
2026-01-14 17:33:18 +0200

    Update hero block to use WordPress media library image

--- template-parts/blocks/hero.php
+++ template-parts/blocks/hero.php
@@ -5,10 +5,9 @@
  * @package GTS
  */
 
-// Get image URL
-$image_url = get_template_directory_uri() . '/assets/media/26_result.webp';
+// Get image URL from WordPress media library
+$image_url = get_site_url() . '/wp-content/uploads/2026/01/26_result-scaled.webp';
 ?>
 
 <section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>'); background-size: cover; background-position: center; background-repeat: no-repeat;">
-	<!-- Debug: <?php echo esc_url( $image_url ); ?> -->
 </section>
