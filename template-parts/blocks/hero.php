1ce5eaf7372ebf49b9703cbe67d72dcd57fc2890
mac <mac@MacBook-Pro-mac.local>
2026-01-14 17:42:52 +0200

    Fix hero block background image display using CSS variable

--- template-parts/blocks/hero.php
+++ template-parts/blocks/hero.php
@@ -9,6 +9,5 @@
 $image_url = get_site_url() . '/wp-content/uploads/2026/01/26_result-scaled.webp';
 ?>
 
-<section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>');">
-	<!-- Test: Image URL = <?php echo esc_url( $image_url ); ?> -->
+<section class="hero-block" style="--bg-image: url('<?php echo esc_url( $image_url ); ?>'); background-image: var(--bg-image);">
 </section>
