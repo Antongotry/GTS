8f7113dc03956ca219cc0929f55af07affe6c4ee
mac <mac@MacBook-Pro-mac.local>
2026-01-14 17:35:38 +0200

    Fix hero block styles - ensure image displays

--- template-parts/blocks/hero.php
+++ template-parts/blocks/hero.php
@@ -9,5 +9,5 @@
 $image_url = get_site_url() . '/wp-content/uploads/2026/01/26_result-scaled.webp';
 ?>
 
-<section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+<section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>');">
 </section>
