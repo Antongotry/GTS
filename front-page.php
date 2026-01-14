e4f8fdf48f9bba504707a5294dbf13650dbd8395
mac <mac@MacBook-Pro-mac.local>
2026-01-14 17:30:30 +0200

    Add hero block with background image (height 780px)

--- front-page.php
+++ front-page.php
@@ -10,6 +10,8 @@ get_header();
 
 <main id="primary" class="site-main">
 
+	<?php get_template_part( 'template-parts/blocks/hero' ); ?>
+
 </main><!-- #primary -->
 
 <?php
---
+++ template-parts/blocks/hero.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Hero Block Template
+ * 
+ * @package GTS
+ */
+
+// Get image URL
+$image_url = get_template_directory_uri() . '/assets/media/26_result.webp';
+?>
+
+<section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>');">
+	<!-- Hero content will be added here -->
+</section>
