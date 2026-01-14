e2ece04d2286eb4a92b1bf08cb143296b005e6a6
mac <mac@MacBook-Pro-mac.local>
2026-01-14 17:31:48 +0200

    Fix hero block background image display

--- template-parts/blocks/hero.php
+++ template-parts/blocks/hero.php
@@ -9,6 +9,5 @@
 $image_url = get_template_directory_uri() . '/assets/media/26_result.webp';
 ?>
 
-<section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>');">
-	<!-- Debug: Image URL = <?php echo esc_url( $image_url ); ?> -->
+<section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>'); background-size: cover; background-position: center; background-repeat: no-repeat;">
 </section>
