4809f25327b2321d2a788421451f920fe306b594
mac <mac@MacBook-Pro-mac.local>
2026-01-15 21:22:55 +0200

    Fix: Fixed card width calculation for exactly 4 cards visible

--- js/trusted-by-slider.js
+++ js/trusted-by-slider.js
@@ -10,30 +10,13 @@
 
 	/* eslint-disable no-new */
 	new Swiper(slider, {
-		slidesPerView: 4,
+		slidesPerView: 'auto',
 		slidesPerGroup: 1,
 		spaceBetween: 20,
 		loop: false,
 		navigation: {
 			nextEl: '.trusted-by-next',
 			prevEl: '.trusted-by-prev',
-		},
-		breakpoints: {
-			1920: {
-				slidesPerView: 4,
-			},
-			1440: {
-				slidesPerView: 3,
-			},
-			1024: {
-				slidesPerView: 2,
-			},
-			768: {
-				slidesPerView: 2,
-			},
-			0: {
-				slidesPerView: 1,
-			},
 		},
 	});
 	/* eslint-enable no-new */
