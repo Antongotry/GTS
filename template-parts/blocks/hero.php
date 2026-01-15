29a8c702af45e1277ddff8fc43572692a62d020e
mac <mac@MacBook-Pro-mac.local>
2026-01-15 17:40:30 +0200

    Update: Change hero background image to hero-baner-1920_result-scaled.webp

--- template-parts/blocks/hero.php
+++ template-parts/blocks/hero.php
@@ -6,7 +6,7 @@
  */
 
 // Get image URL from WordPress media library
-$image_url = get_site_url() . '/wp-content/uploads/2026/01/26_result-scaled.webp';
+$image_url = get_site_url() . '/wp-content/uploads/2026/01/hero-baner-1920_result-scaled.webp';
 ?>
 
 <section class="hero-block" style="background-image: url('<?php echo esc_url( $image_url ); ?>');">
