662a472a77e69011ba483eabc686dd4e5f63e2a8
mac <mac@MacBook-Pro-mac.local>
2026-01-15 21:15:21 +0200

    Update trusted-by block: avatar image, varying heights, new styles

--- template-parts/blocks/trusted-by.php
+++ template-parts/blocks/trusted-by.php
@@ -5,28 +5,33 @@
  * @package GTS
  */
 
+$avatar_url = get_site_url() . '/wp-content/uploads/2026/01/rewievs-ic_result.webp';
+
 $testimonials = array(
 	array(
-		'text' => '“Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum.”',
-		'name' => 'Victoria',
+		'text' => '"Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum. Facilisis aliquet hac vestibulum pulvinar. Ultrices velit a gravida condimentum lectus."',
+		'name' => 'Вікторія',
 	),
 	array(
-		'text' => '“Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum.”',
-		'name' => 'Victoria',
+		'text' => '"Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum. Facilisis aliquet hac vestibulum pulvinar."',
+		'name' => 'Вікторія',
 	),
 	array(
-		'text' => '“Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum.”',
-		'name' => 'Victoria',
+		'text' => '"Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum. Facilisis aliquet hac vestibulum pulvinar. Ultrices velit a gravida condimentum lectus. Morbi et magna."',
+		'name' => 'Вікторія',
 	),
 	array(
-		'text' => '“Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum.”',
-		'name' => 'Victoria',
+		'text' => '"Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum."',
+		'name' => 'Вікторія',
 	),
 	array(
-		'text' => '“Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum.”',
-		'name' => 'Victoria',
+		'text' => '"Convallis interdum mattis morbi ullamcorper. Felis, elementum et, in est fringilla viverra vitae interdum. Facilisis aliquet hac vestibulum pulvinar. Ultrices velit a gravida."',
+		'name' => 'Вікторія',
 	),
 );
+
+// Heights cycle: 348, 326, 392
+$heights = array( 348, 326, 392 );
 ?>
 
 <section class="trusted-by-block">
@@ -40,12 +45,21 @@ $testimonials = array(
 
 		<div class="trusted-by-slider swiper">
 			<div class="swiper-wrapper">
-				<?php foreach ( $testimonials as $testimonial ) : ?>
-					<div class="swiper-slide trusted-by-card">
-						<div class="trusted-by-stars">★★★★★</div>
-						<p class="trusted-by-card-text"><?php echo esc_html( $testimonial['text'] ); ?></p>
+				<?php foreach ( $testimonials as $index => $testimonial ) : ?>
+					<?php $card_height = $heights[ $index % count( $heights ) ]; ?>
+					<div class="swiper-slide trusted-by-card" style="height: <?php echo esc_attr( $card_height ); ?>px;">
+						<div class="trusted-by-card-content">
+							<div class="trusted-by-stars">
+								<span class="trusted-by-star">★</span>
+								<span class="trusted-by-star">★</span>
+								<span class="trusted-by-star">★</span>
+								<span class="trusted-by-star">★</span>
+								<span class="trusted-by-star">★</span>
+							</div>
+							<p class="trusted-by-card-text"><?php echo esc_html( $testimonial['text'] ); ?></p>
+						</div>
 						<div class="trusted-by-author">
-							<span class="trusted-by-avatar" aria-hidden="true"></span>
+							<img src="<?php echo esc_url( $avatar_url ); ?>" alt="" class="trusted-by-avatar" width="56" height="56">
 							<span class="trusted-by-name"><?php echo esc_html( $testimonial['name'] ); ?></span>
 						</div>
 					</div>
