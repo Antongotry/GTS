82dfd4baee41eec4ddb137877cda77f2cd005c41
mac <mac@MacBook-Pro-mac.local>
2026-01-16 18:48:09 +0200

    Add 1440px responsive styles: container 1360px, how-it-works cards 390px, slider 3 cards, footer layout changes

--- js/trusted-by-slider.js
+++ js/trusted-by-slider.js
@@ -18,6 +18,28 @@
 			nextEl: '.trusted-by-next',
 			prevEl: '.trusted-by-prev',
 		},
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+			},
+			// when window width is >= 768px
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			// when window width is >= 1024px
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			// when window width is >= 1441px
+			1441: {
+				slidesPerView: 4,
+				spaceBetween: 20,
+			},
+		},
 	});
 	/* eslint-enable no-new */
 }());
