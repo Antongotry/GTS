05b7a392edf70740276ab5195e2d44309108cada
mac <mac@MacBook-Pro-mac.local>
2026-01-20 00:14:24 +0200

    Create Limousine Service page template
    - Added page-limousine-service.php with hero and booking-form blocks
    - Template is ready for content customization

---
+++ page-limousine-service.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Limousine Service
+ * Template for displaying Limousine Service page
+ *
+ * @package GTS
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+	<?php get_template_part('template-parts/blocks/hero'); ?>
+
+	<?php get_template_part('template-parts/blocks/booking-form'); ?>
+
+</main><!-- #primary -->
+
+<?php
+get_footer();
