27ed6801564bc6313182a5db6de8d109c38a8072
mac <mac@MacBook-Pro-mac.local>
2026-02-23 18:27:06 +0200

    Swap fleet and why-us block order for wedding page

--- single-service.php
+++ single-service.php
@@ -1052,6 +1052,14 @@ $chevron_url = get_template_directory_uri() . '/assets/icons/chevron-down-faq.sv
 		?>
 	<?php endif; ?>
 
+	<?php if ($is_wedding_service && $block_enabled['fleet']) : ?>
+		<?php
+		get_template_part(
+			'template-parts/blocks/fleet-slider'
+		);
+		?>
+	<?php endif; ?>
+
 	<?php if ($is_special_transfers_service && $block_enabled['fleet']) : ?>
 		<?php
 		get_template_part(
@@ -1124,7 +1132,7 @@ $chevron_url = get_template_directory_uri() . '/assets/icons/chevron-down-faq.sv
 		?>
 	<?php endif; ?>
 
-	<?php if ($block_enabled['fleet'] && ! $is_special_transfers_service) : ?>
+	<?php if ($block_enabled['fleet'] && ! $is_special_transfers_service && ! $is_wedding_service) : ?>
 		<?php
 		$fleet_slider_args = array();
 		if ('hourly-hire' === $current_service_slug) {
