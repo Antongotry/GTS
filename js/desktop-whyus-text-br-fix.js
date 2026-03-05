5ccf2d9ad25a87703e0f64580d2594b232a834bf
mac <mac@MacBook-Pro-mac.local>
2026-03-05 14:22:39 +0200

    Make 769-1480 why-us BR cleanup resilient to delayed DOM updates

--- js/desktop-whyus-text-br-fix.js
+++ js/desktop-whyus-text-br-fix.js
@@ -1,18 +1,19 @@
 (function () {
 	'use strict';
 
+	var SELECTOR = '.why-us-block .why-us-item-title, .why-us-block .why-us-item-description';
+
 	function isDesktopAdaptive() {
 		return window.matchMedia('(min-width: 769px) and (max-width: 1480px)').matches;
 	}
 
-	function replaceBrWithSpaces(container) {
-		if (!container) {
+	function replaceBrWithSpaces(element) {
+		if (!element || element.dataset.gtsBrFixed === '1') {
 			return;
 		}
 
-		container.querySelectorAll('br').forEach(function (br) {
-			br.replaceWith(document.createTextNode(' '));
-		});
+		element.innerHTML = element.innerHTML.replace(/<br\s*\/?>/gi, ' ');
+		element.dataset.gtsBrFixed = '1';
 	}
 
 	function applyDesktopWhyUsTextBrFix() {
@@ -20,9 +21,22 @@
 			return;
 		}
 
-		document
-			.querySelectorAll('.why-us-block .why-us-item-title, .why-us-block .why-us-item-description')
-			.forEach(replaceBrWithSpaces);
+		document.querySelectorAll(SELECTOR).forEach(replaceBrWithSpaces);
+	}
+
+	function observeWhyUsMutations() {
+		if (typeof MutationObserver === 'undefined') {
+			return;
+		}
+
+		var observer = new MutationObserver(function () {
+			applyDesktopWhyUsTextBrFix();
+		});
+
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true,
+		});
 	}
 
 	if (document.readyState === 'loading') {
@@ -30,4 +44,13 @@
 	} else {
 		applyDesktopWhyUsTextBrFix();
 	}
+
+	window.addEventListener('load', applyDesktopWhyUsTextBrFix);
+	window.addEventListener('resize', applyDesktopWhyUsTextBrFix, { passive: true });
+
+	// Handle delayed DOM updates from plugins/cached fragments.
+	setTimeout(applyDesktopWhyUsTextBrFix, 300);
+	setTimeout(applyDesktopWhyUsTextBrFix, 1200);
+
+	observeWhyUsMutations();
 })();
